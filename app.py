9ffab882f89a83a27ac952836f5ecb9ed4e9f7a3
Kuber Mehra <kubermehra2000@gmail.com>
2024-12-08 17:33:22 -0500

    Changes in app.py- commenting and adjusting most of the code to mysql
    db.py- integration of mysql
    
    Changes in template

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import datetime
-
 from kafka.metrics.stats import total
 import db
 import json
@@ -100,6 +98,7 @@ def signup():
 
 @app.route('/signin', methods=['GET', 'POST'])
 def signin():
+    
     status, email, username = db.check_user()
     
     data = {
@@ -127,149 +126,149 @@ def display():
     #     return redirect(url_for('home'))  # Redirect to login if no user is logged in
 
 
-@app.route('/generate_list', methods=['GET', 'POST'])
-def get_recommendation():
-    data = check_liked_lables()
-    print("DATA:", data)
-    return json.dumps(data)
-    # return render_template('content.html', data=data)
+# @app.route('/generate_list', methods=['GET', 'POST'])
+# def get_recommendation():
+#     data = check_liked_lables()
+#     print("DATA:", data)
+#     return json.dumps(data)
+#     # return render_template('content.html', data=data)
 
 
-@app.route('/play_video/<videoId>')
-def play_video(videoId):
-    # length_seconds = get_video_length(videoId)
-    # print("LENGTH:",length_seconds)
-    famous_segment = None
-    # Query the most_watched_segment collection to get the famous segment for the given videoId
-    result = most_watched_segment.find_one({"video_id": videoId})
-    if result:
-        famous_segment = result.get("famous_segment")
-    return render_template('play_video.html', videoId=videoId, famous_segment=famous_segment)
+# @app.route('/play_video/<videoId>')
+# def play_video(videoId):
+#     # length_seconds = get_video_length(videoId)
+#     # print("LENGTH:",length_seconds)
+#     famous_segment = None
+#     # Query the most_watched_segment collection to get the famous segment for the given videoId
+#     result = most_watched_segment.find_one({"video_id": videoId})
+#     if result:
+#         famous_segment = result.get("famous_segment")
+#     return render_template('play_video.html', videoId=videoId, famous_segment=famous_segment)
 
 
-# Route to handle liking or disliking a video
-@app.route('/like_video/<video_id>', methods=['POST'])
-def like_video(video_id):
-    action = request.json.get('action')
-    user_email = session.get('email')
+# # Route to handle liking or disliking a video
+# @app.route('/like_video/<video_id>', methods=['POST'])
+# def like_video(video_id):
+#     action = request.json.get('action')
+#     user_email = session.get('email')
     
-    # Check if user exists
-    user_exists = users.find_one({'email': user_email})
-    print("ACTION:", action)
-    print("email:", user_email)
-    if not user_exists:
-        print(3)
-        return jsonify({'error': 'User not found'})
+#     # Check if user exists
+#     user_exists = users.find_one({'email': user_email})
+#     print("ACTION:", action)
+#     print("email:", user_email)
+#     if not user_exists:
+#         print(3)
+#         return jsonify({'error': 'User not found'})
     
-    if action == 'like':
-        print("ADDING")
-        # Update user's liked videos array with video_id
-        users.update_one({'email': user_email}, {'$addToSet': {'liked_videos': video_id}})
-        return jsonify({'message': 'Video liked'})
-    elif action == 'dislike':
-        # Update user's disliked videos array with video_id
-        users.update_one({'email': user_email}, {'$addToSet': {'disliked_videos': video_id}})
-        return jsonify({'message': 'Video disliked'})
-    else:
-        return jsonify({'error': 'Invalid action'})
+#     if action == 'like':
+#         print("ADDING")
+#         # Update user's liked videos array with video_id
+#         users.update_one({'email': user_email}, {'$addToSet': {'liked_videos': video_id}})
+#         return jsonify({'message': 'Video liked'})
+#     elif action == 'dislike':
+#         # Update user's disliked videos array with video_id
+#         users.update_one({'email': user_email}, {'$addToSet': {'disliked_videos': video_id}})
+#         return jsonify({'message': 'Video disliked'})
+#     else:
+#         return jsonify({'error': 'Invalid action'})
 
 
-# ------------------------------------------------------------
-#  Updating current time
-# ------------------------------------------------------------
+# # ------------------------------------------------------------
+# #  Updating current time
+# # ------------------------------------------------------------
 
 
-@app.route('/update_time', methods=['POST'])
-def update_time():
-    VID_SKIP_TIME_TOPIC = 'VID_SKIP_TIME_TOPIC'
+# @app.route('/update_time', methods=['POST'])
+# def update_time():
+#     VID_SKIP_TIME_TOPIC = 'VID_SKIP_TIME_TOPIC'
     
-    user_email = session.get('email')
-    data = request.get_json()
-    video_id = data['video_id']
-    current_time = data['current_time']
-    total_time = data['total_time']
+#     user_email = session.get('email')
+#     data = request.get_json()
+#     video_id = data['video_id']
+#     current_time = data['current_time']
+#     total_time = data['total_time']
     
-    interval_value = total_time / 10
-    interval_number = current_time // interval_value
+#     interval_value = total_time / 10
+#     interval_number = current_time // interval_value
     
-    if current_time == total_time:
-        interval_number = 9
+#     if current_time == total_time:
+#         interval_number = 9
     
-    low_time = interval_number * interval_value
-    high_time = low_time + interval_value
+#     low_time = interval_number * interval_value
+#     high_time = low_time + interval_value
     
-    curTime = datetime.datetime.now()
+#     curTime = datetime.datetime.now()
     
-    if user_email and video_id:
-        push_data = {'timestamp': str(curTime), 'video_id': video_id, 'email': user_email, 'current_time': current_time,
-                     'low_time': low_time, 'high_time': high_time}
-        print('Sending {} to Kafka'.format(push_data))
-        future = kafkaProducer.send(VID_SKIP_TIME_TOPIC, value=push_data)
+#     if user_email and video_id:
+#         push_data = {'timestamp': str(curTime), 'video_id': video_id, 'email': user_email, 'current_time': current_time,
+#                      'low_time': low_time, 'high_time': high_time}
+#         print('Sending {} to Kafka'.format(push_data))
+#         future = kafkaProducer.send(VID_SKIP_TIME_TOPIC, value=push_data)
         
-        r_meta = None
-        try:
-            r_meta = future.get(timeout=10)
-        except KafkaError as e:
-            print('Kafka error when pushing to topic {}: {}'.format(VID_SKIP_TIME_TOPIC, e))
-    else:
-        print('No user or video data')
-    return get200_resp()
+#         r_meta = None
+#         try:
+#             r_meta = future.get(timeout=10)
+#         except KafkaError as e:
+#             print('Kafka error when pushing to topic {}: {}'.format(VID_SKIP_TIME_TOPIC, e))
+#     else:
+#         print('No user or video data')
+#     return get200_resp()
     
-    # Increment the collection in MongoDB for the given video ID and current time frame
-    # Implement your MongoDB logic here
+#     # Increment the collection in MongoDB for the given video ID and current time frame
+#     # Implement your MongoDB logic here
     
-    return jsonify({'message': 'Current time updated successfully'})
+#     return jsonify({'message': 'Current time updated successfully'})
 
 
-# Set up the YouTube Data API client
-# youtube_api_key = 'AIzaSyDlUdNx5_dApzybPBoZhh1HATk-WNP1j5Y'
-# youtube = build('youtube', 'v3', developerKey=youtube_api_key)
+# # Set up the YouTube Data API client
+# # youtube_api_key = 'AIzaSyDlUdNx5_dApzybPBoZhh1HATk-WNP1j5Y'
+# # youtube = build('youtube', 'v3', developerKey=youtube_api_key)
 
-# def get_video_length(video_id):
-#     # Call the YouTube Data API to retrieve video details
-#     request = youtube.videos().list(
-#         part='contentDetails',
-#         id=video_id
-#     )
-#     response = request.execute()
-#     # Extract the duration from the response
-#     duration_str = response['items'][0]['contentDetails']['duration']
-#     # Parse the ISO 8601 duration format to get the length in seconds
-#     duration = isodate.parse_duration(duration_str)
-#     length_seconds = duration.total_seconds()
-#     return length_seconds
+# # def get_video_length(video_id):
+# #     # Call the YouTube Data API to retrieve video details
+# #     request = youtube.videos().list(
+# #         part='contentDetails',
+# #         id=video_id
+# #     )
+# #     response = request.execute()
+# #     # Extract the duration from the response
+# #     duration_str = response['items'][0]['contentDetails']['duration']
+# #     # Parse the ISO 8601 duration format to get the length in seconds
+# #     duration = isodate.parse_duration(duration_str)
+# #     length_seconds = duration.total_seconds()
+# #     return length_seconds
 
 
-# ------------------------------------------------------------
-# Analytics APIs
-# ------------------------------------------------------------
+# # ------------------------------------------------------------
+# # Analytics APIs
+# # ------------------------------------------------------------
 
-@app.route('/ana/mark_opened', methods=['POST'])
-def mark_opened():
-    VID_OPEN_TOPIC = 'VID_OPEN_TOPIC'
-    user_email = session.get('email')
-    req_data = request.get_json()
-    video_id = req_data['video_id']
-    curTime = datetime.datetime.now()
-    users.update_one({'email': user_email},
-                     {'$addToSet': {'watched_history': {'current_time': curTime, 'video_id': video_id}}})
+# @app.route('/ana/mark_opened', methods=['POST'])
+# def mark_opened():
+#     VID_OPEN_TOPIC = 'VID_OPEN_TOPIC'
+#     user_email = session.get('email')
+#     req_data = request.get_json()
+#     video_id = req_data['video_id']
+#     curTime = datetime.datetime.now()
+#     users.update_one({'email': user_email},
+#                      {'$addToSet': {'watched_history': {'current_time': curTime, 'video_id': video_id}}})
     
-    if user_email and video_id:
-        push_data = {'timestamp': str(curTime), 'video_id': video_id, 'email': user_email}
-        print('Sending {} to Kafka'.format(push_data))
-        future = kafkaProducer.send(VID_OPEN_TOPIC, value=push_data)
+#     if user_email and video_id:
+#         push_data = {'timestamp': str(curTime), 'video_id': video_id, 'email': user_email}
+#         print('Sending {} to Kafka'.format(push_data))
+#         future = kafkaProducer.send(VID_OPEN_TOPIC, value=push_data)
         
-        r_meta = None
-        try:
-            r_meta = future.get(timeout=10)
-        except KafkaError as e:
-            print('Kafka error when pushing to topic {}: {}'.format(VID_OPEN_TOPIC, e))
+#         r_meta = None
+#         try:
+#             r_meta = future.get(timeout=10)
+#         except KafkaError as e:
+#             print('Kafka error when pushing to topic {}: {}'.format(VID_OPEN_TOPIC, e))
     
-    return get200_resp()
+#     return get200_resp()
 
 
-def get200_resp():
-    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+# def get200_resp():
+#     return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
 
 if __name__ == '__main__':
--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 import mysql.connector
-# from flask import request, jsonify
+from flask import request, jsonify
+from mysql.connector import cursor
 
 
 
@@ -7,54 +8,48 @@ db=mysql.connector.connect(
     host="localhost",
     user="root",
     passwd="root",
-    database="pds",
-    auth_plugin='mysql_native_password'
+    database="pds"
 )
 
-
-cursor=db.cursor()
-
-cursor.execute("SELECT NOW();")
-
-ans=cursor.fetchone()
+db.autocommit=True
 
 
 
-# # client = pymongo.MongoClient('mongodb://127.0.0.1:27017/')
-# # userdb = client['bigdata']
-# # users = userdb.users
+def insert_data():
+    if request.method == 'POST':
+        username=request.form['username']
+        password = request.form['pass']
+        fname = request.form['fname']
+        lname = request.form['lname']
+        email = request.form['email']
+        phone = request.form['phone']
 
-
-# def insert_data():
-#     if request.method == 'POST':
-#         name = request.form['name']
-#         email = request.form['email']
-#         password = request.form['pass']
         
-#         reg_user = {}
-#         reg_user['name'] = name
-#         reg_user['email'] = email
-#         reg_user['password'] = password
         
-#         if users.find_one({"email": email}) == None:
-#             users.insert_one(reg_user)
-#             return True
-#         else:
-#             return False
+        user=(username,password,fname,lname,email)
+
+        cursor=db.cursor(buffered=True)
+        users=cursor.execute(f"SELECT userName FROM Person where userName = '{username}' ")
+
+        if users == None:
+            query="INSERT INTO Person(userName,password,fname,lname,email) VALUES (%s,%s,%s,%s,%s);"
+            cursor.execute(query,user)
+            return True
+        else:
+            return False
 
 
-# def check_user():
-#     if request.method == 'POST':
-#         email = request.form['email']
-#         password = request.form['pass']
+def check_user():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['pass']
+        print(username)
         
-#         user = {
-#             "email": email,
-#             "password": password
-#         }
-        
-#         user_data = users.find_one(user)
-#         if user_data == None:
-#             return False, ""
-#         else:
-#             return True, user_data["email"], user_data["name"]
+        cursor=db.cursor(dictionary=True)
+
+        user_data=cursor.execute(f"Select * from Person where username='{username}' and password='{password}'")
+        user_data=cursor.fetchone()
+        if user_data == None:
+            return False, "", ""
+        else:
+            return True, user_data["email"], user_data["fname"]
